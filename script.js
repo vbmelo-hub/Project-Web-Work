3e581e7dcde8d12c89b8339f6ea848598ba96299
Vinícius Barros <vinicius.meloin@gmail.com>
2025-02-09 17:51:50 -0500

    version 1.0

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
     function checkScroll() {
         const triggerBottom = window.innerHeight * 0.8;
 
-        events.forEach(event => {
+        events.forEach((event, index) => {
             const eventTop = event.getBoundingClientRect().top;
+
             if (eventTop < triggerBottom) {
                 event.style.opacity = "1";
                 event.style.transform = "translateX(0)";
             }
         });
+
+     // Ativar os últimos itens mesmo sem scroll extra
+     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+        events.forEach(event => {
+            event.style.opacity = "1";
+            event.style.transform = "translateX(0)";
+        });
     }
+}
 
     window.addEventListener("scroll", checkScroll);
     checkScroll(); // Executa no carregamento da página
